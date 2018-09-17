e1a8d99e92ccb676a6bf26850e0d9a73195aa5d5
Tony Zurolo <tzurolo@gmail.com>
2018-09-16 21:43:10 -0400

    added simple "Kalman" filtering of sensor data

--- web/WaterLevelMonitorServer.js
+++ web/WaterLevelMonitorServer.js
@@ -4,8 +4,9 @@ var net = require('net');
 var readline = require('readline');
 
 var HOST = 'localhost';
-var PORT = 3000;
+var SENSOR_PORT = 3000;
 var ThingSpeakSensorWritekey = "DD7TVSCZEHKZLAQP";
+var DISPLAY_PORT = 3010;
 var ThingSpeakPumpWritekey = "P55J3PTLW77TJFLB";
 
 var mainsock;
@@ -13,9 +14,11 @@ var pendingCommand = '';
 
 var tankEmptyDistance = 300;
 var tankFullDistance = 30;
-var latestWaterLevel = -1;  // -1 means unknown
-var latestWaterLevelTimestamp = 0;
 
+var latestValidWaterDistance = { "distance" : -1, "timestamp" : 0};  // -1 means unknown
+var latestWaterLevel = { "level" : -1, "timestamp" : 0};  // -1 means unknown
+
+var LoJASensorUnitId = 2;
 var LoJADisplayUnitId = 3;
 
 var gpsStart = Date.UTC(1980, 0, 6);
@@ -29,6 +32,32 @@ function waterLevelFromDistance (distance)
     return Math.round(
         ((tankEmptyDistance - distance) * 100) /
         (tankEmptyDistance - tankFullDistance));
+}
+
+var maxFillRate = 0.0175;    // cm per second
+var maxDrainRate = 0.0175;   // cm per second
+function computeDistanceBounds (sampleTimestamp, latestValidDistance)
+{
+	// default: full range
+	var bounds = { "low" : tankFullDistance,
+				  "high" : tankEmptyDistance};
+	if (latestValidDistance.distance >= 0) {
+		var timeInterval = sampleTimestamp - latestValidDistance.timestamp;  // seconds
+		var latestDistance = latestValidDistance.distance;
+
+		bounds.low = latestDistance - maxFillRate * timeInterval;
+		if (bounds.low < tankFullDistance) bounds.low = tankFullDistance;
+
+	    bounds.high = latestDistance + maxDrainRate * timeInterval;
+		if (bounds.high > tankEmptyDistance) bounds.high = tankEmptyDistance;
+	}
+
+	return bounds;
+}
+
+function isWithinBounds (value, bounds)
+{
+	return (value >= bounds.low) && (value <= bounds.high);
 }
 
 //
@@ -129,6 +158,7 @@ var sensorFieldDescriptors = {
    "B" : {fieldName : "field3",   divisor : 100 },
    "Q" : {fieldName : "field4",   divisor : 1   },
    "C" : {fieldName : "field5",   divisor : 1   },
+   "I" : {fieldName : "id",       divisor : 1   }
    };
 
 // sets a field of sample from the given fieldStr. fieldStr
@@ -152,13 +182,47 @@ function parseSensorDataFeed(sensorDataStr) {
         var sampleStr = packetStrs[i];
         var sample = {"delta_t" : 0};
         var sampleFields = sampleStr.match(fieldRE);
-        for (x in sampleFields) {
-           parseField(sampleFields[x], sensorFieldDescriptors, sample);
+        for (f in sampleFields) {
+           parseField(sampleFields[f], sensorFieldDescriptors, sample);
         }
-		var waterLevel = waterLevelFromDistance(sample.field1);
-		sample.field6 = waterLevel.toString();
         samples.push(sample);
     }
+
+	// set timestamps on samples
+	var sampleTime = gpsTime(new Date());
+	for (i = samples.length - 1; i >= 0; --i) {
+		samples[i].abs_t = sampleTime;
+		sampleTime -= samples[i].delta_t;
+	}
+
+	// filter samples with simple Kalman-like filter.
+	// for good samples compute water level from distance to water.
+	for (s in samples) {
+		// filter sample
+		var distance = samples[s].field1;
+		var sampleTime = samples[s].abs_t;
+		var bounds = computeDistanceBounds(sampleTime, latestValidWaterDistance);
+		//console.log("Distance: " + distance + ", bounds: " + bounds.low + ".." + bounds.high);
+		if (isWithinBounds(distance, bounds)) {
+			// valid sample
+			// update filter state
+			latestValidWaterDistance.distance = distance;
+			latestValidWaterDistance.timestamp = sampleTime;
+			
+			// compute and capture water level for display
+			waterLevel = waterLevelFromDistance(distance);
+			latestWaterLevel.level = waterLevel;
+			latestWaterLevel.timestamp = samples[s].abs_t;
+			
+			samples[s].field6 = waterLevel;
+			
+			// clear out abs_t - it's only needed for this filter
+			delete samples[s].abs_t;
+		} else {
+			console.log('>>> rejecting sample distance ' + distance + ", bounds: " + bounds.low + ".." + bounds.high);
+		}
+	}
+	
     // set connection data on last sample
     var lastSample = samples[samples.length-1];
     var connFields = connDataStr.match(fieldRE);
@@ -166,11 +230,13 @@ function parseSensorDataFeed(sensorDataStr) {
         parseField(connFields[x], sensorFieldDescriptors, lastSample);
     }
     
-    // capture water level to send to display
-    latestWaterLevel = lastSample.field6;
-    
-    // send to ThingSpeak
-    postBulkDataToThingSpeak(samples);
+    if (lastSample.id == LoJASensorUnitId) {
+        // send to ThingSpeak
+		delete lastSample.id; // we don't post the id
+        postBulkDataToThingSpeak(samples);
+    } else {
+        console.log('got data from alternate sensor ' + lastSample.id);
+    }
 }
 //
 // end of parse sensor data and send to ThingSpeak
@@ -185,39 +251,38 @@ var waterLevelMonitorServer = net.createServer(function(sock) {
     var now = new Date();
     console.log('CONNECTED ' + now.toDateString() + " " + now.toLocaleTimeString() + ': ' +
         sock.remoteAddress +':'+ sock.remotePort);
-    var setTimeCmd = 'tset ' + gpsTime(now);
-    if (pendingCommand.length > 0) {
-        console.log('sending block start');
-        sock.write('[\r');
-        pendingCommand = '';
-    }
-    sock.write(setTimeCmd + '\r');
 
-    sock["mycounter"] = 0;	
     sock["incomingData"] = '';
+    sock["incomingDataLineNumber"] = 0;
     mainsock = sock;
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
-        sock.mycounter++;
         var dataString = data.toString('utf8');
         var now = new Date();
         console.log('DATA ' + now.toDateString() + " " + now.toLocaleTimeString() + ': ' +
-            dataString + ', count: ' + sock.mycounter);
-        latestWaterLevelTimestamp = gpsTime(now);
-        if (sock.mycounter == 1) {
-            // first line is sensor data
-            // validate
-            if ((dataString.charAt(0) == 'I') && 
-                ((dataString.charAt(dataString.length-2) == 'Z') ||
-                 (dataString.charAt(dataString.length-1) == 'Z'))) {
-                // parse the data
-                parseSensorDataFeed(dataString);
-                
+            dataString + ', line: ' + sock.incomingDataLineNumber);
+        for (chIndex in dataString) {
+            if (dataString.charCodeAt(chIndex) == 10) {
+                // end of line
+                sock.incomingDataLineNumber++;
+                if (sock.incomingDataLineNumber == 1) {
+                    // first line is sensor data
+                    // validate
+                    if ((sock.incomingData.charAt(0) == 'I') && 
+                        (sock.incomingData.charAt(sock.incomingData.length-1) == 'Z')) {
+                        // parse the data
+                        parseSensorDataFeed(sock.incomingData);
+                    } else {
+                        console.log('>>>> unexpected input - destroy socket');
+                        sock.destroy('unexpected');
+                        break;
+                    }
+                }
+                sock.incomingData = '';
             } else {
-                console.log('>>>> unexpected input - destroy socket');
-                sock.destroy('unexpected');
+                sock.incomingData += dataString.charAt(chIndex);
             }
-        }        
+        }                
     });
     
     // Add a 'close' event handler to this instance of socket
@@ -235,7 +300,14 @@ var waterLevelMonitorServer = net.createServer(function(sock) {
             exception);
         pendingCommand = '';
     });
-    
+
+    var setTimeCmd = 'tset ' + gpsTime(now);
+    if (pendingCommand.length > 0) {
+        console.log('sending block start');
+        sock.write('[\r');
+        pendingCommand = '';
+    }
+    sock.write(setTimeCmd + '\r');
 });
 //
 //  end of water level monitor net server
@@ -284,38 +356,38 @@ var waterLevelDisplayServer = net.createServer(function(sock) {
     var now = new Date();
     console.log('display CONNECTED ' + now.toDateString() + " " + now.toLocaleTimeString() + ': ' +
         sock.remoteAddress +':'+ sock.remotePort);
-    var dataCmd = 'data ' + latestWaterLevel + ' ' + latestWaterLevelTimestamp + ' ' + gpsTime(now);
-    console.log(dataCmd);
-    if (pendingCommand.length > 0) {
-        console.log('sending block start');
-        sock.write('[\r');
-        pendingCommand = '';
-    }
-    sock.write(dataCmd + '\r');
 
-    sock["mycounter"] = 0;
     sock["incomingData"] = '';
+    sock["incomingDataLineNumber"] = 0;
     mainsock = sock;
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
-        sock.mycounter++;
         var dataString = data.toString('utf8');
         var now = new Date();
         console.log('display DATA ' + now.toDateString() + " " + now.toLocaleTimeString() + ': ' +
-            dataString + ', count: ' + sock.mycounter);
-        if (sock.mycounter == 1) {
-            // first line is sensor data
-            // validate
-            if ((dataString.charAt(0) == 'I') && 
-                ((dataString.charAt(dataString.length-2) == 'Z') ||
-                 (dataString.charAt(dataString.length-1) == 'Z'))) {
-                // parse the data
-                parsePumpDataFeed(dataString);                
+            dataString + ', line: ' + sock.incomingDataLineNumber);
+        for (chIndex in dataString) {
+            if (dataString.charCodeAt(chIndex) == 10) {
+                // end of line
+                sock.incomingDataLineNumber++;
+                if (sock.incomingDataLineNumber == 1) {
+                    // first line is sensor data
+                    // validate
+                    if ((sock.incomingData.charAt(0) == 'I') && 
+                        (sock.incomingData.charAt(sock.incomingData.length-1) == 'Z')) {
+                        // parse the data
+                        parsePumpDataFeed(sock.incomingData);
+                    } else {
+                        console.log('>>>> unexpected input - destroy socket');
+                        sock.destroy('unexpected');
+                        break;
+                    }
+                }
+                sock.incomingData = '';
             } else {
-                console.log('>>>> unexpected input - destroy socket');
-                sock.destroy('unexpected');
+                sock.incomingData += dataString.charAt(chIndex);
             }
-        }        
+        }                
     });
     
     // Add a 'close' event handler to this instance of socket
@@ -334,6 +406,14 @@ var waterLevelDisplayServer = net.createServer(function(sock) {
         pendingCommand = '';
     });
     
+    var dataCmd = 'data ' + latestWaterLevel.level + ' ' + latestWaterLevel.timestamp + ' ' + gpsTime(now);
+    console.log(dataCmd);
+    if (pendingCommand.length > 0) {
+        console.log('sending block start');
+        sock.write('[\r');
+        pendingCommand = '';
+    }
+    sock.write(dataCmd + '\r');
 });
 //
 //  end of net server
@@ -370,7 +450,7 @@ function readLatestWaterLevelFromThingSpeak() {
     var get_options = {
         host: 'api.thingspeak.com',
         port: '80',
-        path: '/channels/8203/fields/6.json?results=1',
+        path: '/channels/8203/feeds.json?results=100',
         method: 'GET',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -385,25 +465,48 @@ function readLatestWaterLevelFromThingSpeak() {
           responseData += chunk;
       });
       res.on('end', function () {
-          // console.log('Response: ' + responseData);
-          responseObj = JSON.parse(responseData);
-          latestWaterLevel = parseInt(responseObj.feeds[0].field6);
-          console.log('Water Level: ' + latestWaterLevel);
-          var latestWaterLevelDate = new Date(responseObj.feeds[0].created_at);
-          console.log('      as of: ' + latestWaterLevelDate);
-          latestWaterLevelTimestamp = gpsTime(latestWaterLevelDate);
+         // console.log('Response: ' + responseData);
+         responseObj = JSON.parse(responseData);
+
+		// search backwards for the most recent non-null water level field
+		var searchIndex = responseObj.feeds.length - 1;
+		while ((searchIndex >= 0) &&
+		       (responseObj.feeds[searchIndex].field6 == null)) {
+			--searchIndex;
+		}
+		  
+		if (searchIndex >= 0) {
+			// found non-null water level field
+			var waterLevelData = responseObj.feeds[searchIndex];
+
+			// set latest water level
+            latestWaterLevel.level = parseInt(waterLevelData.field6);
+            console.log('   Water Level: ' + latestWaterLevel.level);
+            var latestWaterLevelDate = new Date(waterLevelData.created_at);
+            latestWaterLevel.timestamp = gpsTime(latestWaterLevelDate);
+
+			// set latest valid water distance
+			latestValidWaterDistance.distance = parseFloat(waterLevelData.field1);
+            console.log('Water Distance: ' + latestValidWaterDistance.distance);
+			latestValidWaterDistance.timestamp = latestWaterLevel.timestamp;
+            console.log('         as of: ' + latestWaterLevelDate);
+		} else {
+			// did not find water level field
+			latestWaterLevel.level = -1;
+			latestWaterLevel.timestamp = 0;
+		}
       });
   });
   
   // get the data
-  console.log("Getting field from ThingSpeak");
+  console.log("Getting latest data from ThingSpeak");
   get_req.end();
 }
 readLatestWaterLevelFromThingSpeak();
 
-waterLevelMonitorServer.listen(3000, function() {
-  console.log('water level monitor server bound');
+waterLevelMonitorServer.listen(SENSOR_PORT, function() {
+  console.log('water level monitor server bound on port ' + SENSOR_PORT);
 });
-waterLevelDisplayServer.listen(3010, function() {
-  console.log('water level display server bound');
+waterLevelDisplayServer.listen(DISPLAY_PORT, function() {
+  console.log('water level display server bound on port ' + DISPLAY_PORT);
 });
